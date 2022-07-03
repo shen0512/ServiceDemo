103f48f388cac587a7c179199c06184958c0b4d9
shen0512 <shen0512@github.com>
2022-07-03 20:26:41 +0800

    init

---
+++ src/main/java/com/example/demo/TestREST.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+@RestController
+public class TestREST {
+	public class ResponseStatus{
+		private String status;
+		private String msg;
+		private String msgFromClient;
+		
+		public ResponseStatus(String status, String msg, String msgFromClient) {
+			this.status = status;
+			this.msg = msg;
+			this.msgFromClient = msgFromClient;
+		}
+	}
+	
+	@PostMapping("/test")
+	@ResponseBody
+    public Object get(@RequestBody Object requestBody) {
+		Gson gson = new Gson();
+		String requestBodyStr = gson.toJson(requestBody);
+		
+		JsonObject jsonObject = JsonParser.parseString(requestBodyStr).getAsJsonObject();
+		try {
+			String tmp = jsonObject.get("msg").getAsString();
+			ResponseStatus r = new ResponseStatus("accept","", tmp);
+			return gson.toJson(r);
+		}catch(Exception e) {
+			ResponseStatus r = new ResponseStatus("error","msg not found", "");
+			return gson.toJson(r);
+		}
+		
+    }
+}
